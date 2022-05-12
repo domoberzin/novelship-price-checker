a26eee5d88b6818535cd4a12d71e763638f075cd
domoberzin <74132255+domoberzin@users.noreply.github.com>
2022-05-12 19:50:37 +0800

    Update novelship.py

--- novelship.py
+++ novelship.py
@@ -5,8 +5,10 @@ from copy import deepcopy
 
 def search_novel(search_query):
     search_query=(str(search_query)).replace(" ","%20")
-    search_url = "https://novelship.com/api/products/search?where[search]=" +search_query+ "&where[active:eq]=true&page[number]=0&page[size]=20"
-    headers_dict = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"}
+    search_url = "https://novelship.com/api/products/search?where[search]=" + 
+    search_query + "&where[active:eq]=true&page[number]=0&page[size]=20"
+    headers_dict = {"user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) 
+                    AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36"}
     hook = Webhook("insert-webhook-here")
 
 
@@ -20,7 +22,8 @@ def search_novel(search_query):
         pid = pid[0:5] #probably need to fix this
         name = search["results"][0]["name"]
         name_slug = search["results"][0]["name_slug"]
-        a = requests.get("https://novelship.com/api/products/slug/"+ name_slug + "?where[active:eq]=true",headers=headers_dict).json()
+        a = requests.get("https://novelship.com/api/products/slug/"+ 
+                         name_slug + "?where[active:eq]=true",headers=headers_dict).json()
         print(a.keys())
         size_list_bids = a["sizes"]
         size_list_asks = deepcopy(size_list_bids)
@@ -31,7 +34,8 @@ def search_novel(search_query):
             sku = "(SKU not available)"
         image_url = search["results"][0]["image"]
 
-        found_url =  "https://novelship.com/api/products/" + pid +  "/offer-lists?where[active:eq]=true&modifier[c]=3&page[number]=0&page[size]=1000"
+        found_url =  "https://novelship.com/api/products/" + pid +  
+                    "/offer-lists?where[active:eq]=true&modifier[c]=3&page[number]=0&page[size]=1000"
         found = requests.get(found_url,headers=headers_dict).json()
         print(found)
         buying_price_list = []
@@ -40,19 +44,23 @@ def search_novel(search_query):
             try:
                 if found["results"][i]["type"] == "buying":
                     price = found["results"][i]["price"]
-                    buying_price_list.append(((str(found["results"][i]["size"])),int(f"{price:.0f}"),str((f"{(price*0.884- 6.80 - 0.70):.0f}")),(float(found["results"][i]["size"][3:]))))
+                    buying_price_list.append(((str(found["results"][i]["size"])),int(f"{price:.0f}"), 
+                                              str((f"{(price*0.884- 6.80 - 0.70):.0f}")),(float(found["results"][i]["size"][3:]))))
                 if found["results"][i]["type"] == "selling":
                     price2= found["results"][i]["price"]
-                    selling_price_list.append(((str(found["results"][i]["size"])),int(f"{price2:.0f}"),str((f"{(price2*0.884- 6.80 - 0.70):.0f}")),(float(found["results"][i]["size"][3:]))))
+                    selling_price_list.append(((str(found["results"][i]["size"])),int(f"{price2:.0f}"), 
+                                               str((f"{(price2*0.884- 6.80 - 0.70):.0f}")),(float(found["results"][i]["size"][3:]))))
                 sorted_bids = sorted(buying_price_list,key =lambda x:x[3])
                 sorted_asks2 = sorted(selling_price_list,key =lambda x:x[3])
             except ValueError:
                 if found["results"][i]["type"] == "buying":
                     price = found["results"][i]["price"]
-                    buying_price_list.append(((str(found["results"][i]["size"])),int(f"{price:.0f}"),str((f"{(price*0.884- 6.80 - 0.70):.0f}"))))
+                    buying_price_list.append(((str(found["results"][i]["size"])),int(f"{price:.0f}"), 
+                                              str((f"{(price*0.884- 6.80 - 0.70):.0f}"))))
                 if found["results"][i]["type"] == "selling":
                     price2= found["results"][i]["price"]
-                    selling_price_list.append(((str(found["results"][i]["size"])),int(f"{price2:.0f}"),str((f"{(price2*0.884- 6.80 - 0.70):.0f}"))))
+                    selling_price_list.append(((str(found["results"][i]["size"])),int(f"{price2:.0f}"), 
+                                               str((f"{(price2*0.884- 6.80 - 0.70):.0f}"))))
                 sorted_bids = sorted(buying_price_list,key =lambda x:x[1])
                 sorted_asks2 = sorted(selling_price_list,key =lambda x:x[1])
 
